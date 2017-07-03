e6c622b36b705c7eb2e2fb3ce1cbe8d37f9ecf32
Ayuni Arfina <ayuniarfina06@gmail.com>
2017-07-03 14:28:28 +0700

    controller

--- app/Http/Controllers/donasiController.php
+++ app/Http/Controllers/donasiController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\donasi;
 use Illuminate\Http\Request;
 
 class donasiController extends Controller
 {
-    //
+	public function show($id)
+	{
+		return view('donasi.store', ['donasi_uang' => Donasi_uang::findOrFail($id)]);
+	}
 }
