e3df35104dab30dcef223c77950cd306e61bd9eb
nblskr <nabila.s.ramadhanti@gmail.com>
2017-07-12 00:47:14 +0700

    route

--- resources/views/front/template.blade.php
+++ resources/views/front/template.blade.php
@@ -12,7 +12,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/">Panti Kita</a>
+      <a class="navbar-brand" href="/home">Panti Kita</a>
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
