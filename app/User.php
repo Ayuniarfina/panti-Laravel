3f57347dfb9b9ec7210093d6c2f5563a07225227
Ayuni Arfina <ayuniarfina06@gmail.com>
2017-07-11 23:37:41 +0700

    controllerlagi

--- app/User.php
+++ app/User.php
@@ -7,16 +7,16 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'nama', 'email', 'password',
     ];
+
+    protected $connection = 'mysql';
 
     /**
      * The attributes that should be hidden for arrays.
